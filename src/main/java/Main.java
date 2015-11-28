33a3d424ae036331809f113ac9412d70cf415fad
Lenok <elena_budyukina@mail.ru>
2015-11-28 14:14:15 +0300

    ....

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,26 @@
+import java.awt.*;
 import java.util.Vector;
 import  java.io.PrintWriter;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Neikila on 07.11.2015.
  */
 public class Main {
     public static void main(String[] args) throws Exception{
+
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                SimpleFrame frame = new SimpleFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+
+
+
+
         Music test = new Music();
 
 //        testAnalyzator();
@@ -22,6 +37,30 @@ public class Main {
         analizator.analize(testMusic, testText);
         Vector<Integer> endVector = analizator.getEndVector();
         int N = 25;
+
+//        System.out.println();
+//        Complex[] x = new Complex[4];
+//        x[0] = new Complex(0, 1);
+//        x[1] = new Complex(2, 3);
+//        x[2] = new Complex(4, 5);
+//        x[3] = new Complex(6, 7);
+//
+//        for (int i = 0; i < x.length; ++i){
+//            System.out.println(x[i]);
+//        }
+//
+//        Complex[] y = FFT.fft(x);
+//        Complex[] z = FFT.ifft(y);
+//
+//        System.out.println();
+//        for (int i = 0; i < y.length; ++i){
+//            System.out.println(y[i]);
+//        }
+//        System.out.println();
+//        for (int i = 0; i < z.length; ++i){
+//            System.out.println(z[i]);
+//        }
+
 
 
 
---
+++ src/main/java/SimpleFrame.java
@@ -0,0 +1,118 @@
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by elena on 28.11.15.
+ */
+public class SimpleFrame extends JFrame {
+    public SimpleFrame(){
+      //  определение размеров экрана
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        //Установка ширины и высоты фрейма
+        // и позиционирвание с помощью платформы
+        setSize(screenWidth / 2, screenHeight / 2);
+        setLocationByPlatform(true);
+        setLocation(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        //Установка пиктограммы и заголовка окна
+       // Image img = kit.getImage("icon.gif");
+       // setIconImage(img);
+        setTitle("тра-ла-ла");
+//            setDefaultCloseOperation(EXIT_ON_CLOSE);
+          //  ImageIcon icon = new ImageIcon("1.gif"); // будем использовать один значок на все случаи
+            Box mainBox = Box.createVerticalBox();
+            Box box1 = Box.createVerticalBox();
+
+        buttonPanel = new JPanel();
+            JToggleButton tButton1 = new JToggleButton("Кнопка выбора 1");
+            JToggleButton tButton2 = new JToggleButton("Кнопка выбора 2");
+            ButtonGroup bg = new ButtonGroup(); // создаем группу взаимного исключения
+            bg.add(tButton1);
+            bg.add(tButton2); // сделали кнопки tButton1 и tButton2 взаимоисключающими
+            box1.add(tButton1);
+            box1.add(tButton2); // добавили кнопки tButton1 и tButton2 на панель box1
+            box1.setBorder(new TitledBorder("Выбор"));
+//            Box box2 = Box.createVerticalBox();
+//            JCheckBox check1 = new JCheckBox("Флажок 1");
+//            JCheckBox check2 = new JCheckBox("Флажок 2");
+//            box2.add(check1);
+//            box2.add(check2); // добавили флажки на панель box2
+//            box2.setBorder(new TitledBorder("Флажки"));
+//            Box box3 = Box.createVerticalBox();
+//            JRadioButton rButton1 = new JRadioButton("Переключатель 1");
+//            JRadioButton rButton2 = new JRadioButton("Переключатель 2");
+//            bg = new ButtonGroup(); // создаем группу взаимного исключения
+//
+//            bg.add(rButton1);
+//            bg.add(rButton2); // сделали радиокнопки взаимоисключающими
+//            box3.add(rButton1);
+//            box3.add(rButton2); // добавили радиокнопки на панель box3
+//            box3.setBorder(new TitledBorder("Переключатели"));
+            mainBox.add(box1);
+//            mainBox.add(box2);
+//            mainBox.add(box3);
+            setContentPane(mainBox);
+
+//            pack();
+        }
+    private JPanel buttonPanel;
+
+    public static final int DEFAULT_WIDTH = 300;
+    public static final int DEFAULT_HEIGHT = 200;
+    }
+//        //определение размеров экрана
+//        Toolkit kit = Toolkit.getDefaultToolkit();
+//        Dimension screenSize = kit.getScreenSize();
+//        int screenHeight = screenSize.height;
+//        int screenWidth = screenSize.width;
+//        //Установка ширины и высоты фрейма
+//        // и позиционирвание с помощью платформы
+//        setSize(screenWidth / 2, screenHeight / 2);
+//        setLocationByPlatform(true);
+//        setLocation(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+//        //Установка пиктограммы и заголовка окна
+//        Image img = kit.getImage("icon.gif");
+//        setIconImage(img);
+//        setTitle("тра-ла-ла");
+//
+//        buttonPanel = new JPanel();
+//
+//
+//
+//
+//        JButton yellow = new JButton("Yellow");
+//        JButton blue = new JButton("Blue");
+//
+//        buttonPanel = new JPanel();
+//        buttonPanel.add(yellow);
+//        buttonPanel.add(blue);
+//        add(buttonPanel);
+//
+//        //создание действий кнопок
+//        CAction yellowAction = new CAction(Color.YELLOW);
+//        CAction blueAction = new CAction(Color.BLUE);
+//
+//        //связывание действий с кнопками
+//        yellow.addActionListener(yellowAction);
+//        blue.addActionListener(blueAction);
+//    }
+//    private class CAction implements ActionListener{
+//        public CAction(Color c){
+//            backgroundColor = c;
+//        }
+//        public void actionPerformed (ActionEvent event){
+//            buttonPanel.setBackground(backgroundColor);
+//        }
+//        private Color backgroundColor;
+//    }
+//
+//    private JPanel buttonPanel;
+//
+//    public static final int DEFAULT_WIDTH = 300;
+//    public static final int DEFAULT_HEIGHT = 200;
