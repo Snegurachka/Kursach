59848d087cc364ae06aaed9d8210e03da113b8f0
Kirill <neikila@gmail.com>
2015-11-28 23:02:35 +0300

    Немножко поменял название переменных и добавил сохранение путей в абсолютных значениях

--- src/main/java/Frame1.java
+++ src/main/java/Frame1.java
@@ -1,26 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.Vector;
-import  java.io.PrintWriter;
+
 /**
  * Created by elena on 28.11.15.
  */
 
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
 public class Frame1 extends JFrame {
+
+    public static final int DEFAULT_WIDTH = 300;
+    public static final int DEFAULT_HEIGHT = 200;
+
+    private String absolutePathToAudioFile;
+    private String absolutePathToTextFile;
 
     public Frame1() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,82 +26,90 @@ public class Frame1 extends JFrame {
 
 //        panel.add(Box.createVerticalGlue());
 
-        final JLabel label = new JLabel("аудио");
-//        label.setAlignmentX(CENTER_ALIGNMENT);
-        label.setSize(200, 50);
-        label.setLocation(70, 20);
-        panel.add(label);
+        final JLabel audioLabel = new JLabel("аудио");
+//        audioLabel.setAlignmentX(CENTER_ALIGNMENT);
+        audioLabel.setSize(200, 50);
+        audioLabel.setLocation(70, 20);
+        panel.add(audioLabel);
 
 //        panel.add(Box.createRigidArea(new Dimension(10, 10)));
 
-        JButton button = new JButton("Выбрать аудио");
-        button.setSize(200, 50);
-        button.setLocation(40, 70);
-        panel.add(button);
+        JButton audioButton = new JButton("Выбрать аудио");
+        audioButton.setSize(200, 50);
+        audioButton.setLocation(40, 70);
+        panel.add(audioButton);
 
-//        button.setAlignmentX(CENTER_ALIGNMENT);
+//        audioButton.setAlignmentX(CENTER_ALIGNMENT);
 
-        final JLabel label2 = new JLabel("текст");
-//        label.setAlignmentX(CENTER_ALIGNMENT);
-        label2.setSize(200, 50);
-        label2.setLocation(70, 150);
-        panel.add(label2);
+        final JLabel textLabel = new JLabel("текст");
+//        audioLabel.setAlignmentX(CENTER_ALIGNMENT);
+        textLabel.setSize(200, 50);
+        textLabel.setLocation(70, 150);
+        panel.add(textLabel);
 
 //        panel.add(Box.createRigidArea(new Dimension(10, 10)));
 
-        JButton button2 = new JButton("Выбрать текст");
-        button2.setSize(200, 50);
-        button2.setLocation(40, 200);
-        panel.add(button2);
+        JButton textButton = new JButton("Выбрать текст");
+        textButton.setSize(200, 50);
+        textButton.setLocation(40, 200);
+        panel.add(textButton);
 
-        JButton button3 = new JButton("Решить");
-        button3.setSize(400, 100);
-        button3.setLocation(300, 300);
-        panel.add(button3);
+        JButton solveButton = new JButton("Решить");
+        solveButton.setSize(400, 100);
+        solveButton.setLocation(300, 300);
+        panel.add(solveButton);
 
 
-        button.addActionListener(new ActionListener() {
+        audioButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 JFileChooser fileopen = new JFileChooser();
                 int ret = fileopen.showDialog(null, "Открыть файл");
                 if (ret == JFileChooser.APPROVE_OPTION) {
                     File file = fileopen.getSelectedFile();
-                    label.setText(file.getName());
+                    absolutePathToAudioFile = file.getAbsolutePath();
+                    audioLabel.setText(file.getName());
                 }
             }
         });
 
-        button2.addActionListener(new ActionListener() {
+        textButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 JFileChooser fileopen = new JFileChooser();
                 int ret = fileopen.showDialog(null, "Открыть файл");
                 if (ret == JFileChooser.APPROVE_OPTION) {
                     File file = fileopen.getSelectedFile();
-                    label2.setText(file.getName());
+                    textLabel.setText(file.getName());
+                    absolutePathToTextFile = file.getAbsolutePath();
                 }
             }
         });
 
-        button3.addActionListener(new ActionListener() {
+        solveButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Music test = new Music();
                 Vector<Integer> testText;
                 String textName = "testkurs.txt";
                 TextReader textreader = new TextReader();
+                try {
+                    testText = textreader.readFile(textName);
+//                    testText = textreader.readFile(absolutePathToTextFile);
+                    String filename = "vals.mp3";
 
-                testText = textreader.readFile(textName);
-                String filename = "vals.mp3";
+                    Vector<Integer> testMusic;
+                    testMusic = test.getData(filename);
+//                    testMusic = test.getData(absolutePathToAudioFile);
 
-                Vector<Integer> testMusic;
-                testMusic = test.getData(filename);
-
-                Analizator analizator = new Analizator();
-                analizator.analize(testMusic, testText);
-                Vector<Integer> endVector = analizator.getEndVector();
+                    Analizator analizator = new Analizator();
+                    analizator.analize(testMusic, testText);
+                    Vector<Integer> endVector = analizator.getEndVector();
+                } catch (Exception error) {
+                    System.out.println(error);
+                    System.out.println("Ошибка при чтении в TextReader");
+                }
             }
         });
 
-//        panel.add(button);
+//        panel.add(audioButton);
 //        panel.add(Box.createVerticalGlue());
         getContentPane().add(panel);
 
@@ -128,9 +131,6 @@ public class Frame1 extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
     }
-
-    public static final int DEFAULT_WIDTH = 300;
-    public static final int DEFAULT_HEIGHT = 200;
 //    public static void main(String[] args) {
 //        javax.swing.SwingUtilities.invokeLater(new Runnable() {
 //            public void run() {
@@ -140,6 +140,14 @@ public class Frame1 extends JFrame {
 //            }
 //        });
 //    }
+
+    public String getAbsolutePathToAudioFile() {
+        return absolutePathToAudioFile;
+    }
+
+    public String getAbsolutePathToTextFile() {
+        return absolutePathToTextFile;
+    }
 }
 
 
